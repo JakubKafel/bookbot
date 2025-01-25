1880247c498a33dd63dddb7315e532db09feb283
jkafel <jakubkkafel@gmail.com>
2025-01-25 12:11:48 +0100

    project finished, added report with word and char count

--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-def main():
-    book_path = "/root/workspace/github.com/JakubKafel/bookbot/books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    #print(f"{num_words} words found in the document")
-    charater_dictionary = count_characters(text)
-    print(charater_dictionary)
-
-
 def get_num_words(text):
     words = text.split()
     return len(words)
@@ -26,6 +17,37 @@ def count_characters(text):
 
     return char_count
 
+def sort_on(dict):
+    return dict["num"]
+
+def report(text,book_path):
+
+    char_dict = count_characters(text)
+    sorted_chars_dict = []
+    num_words = get_num_words(text)
+    for char in char_dict:
+        if char.isalpha():
+            sorted_chars_dict.append({"char":char,"num":char_dict[char]})
+
+    sorted_chars_dict.sort(reverse=True,key=sort_on)
     
+
+    print(f"--- Begin report of {book_path} ---\n")
+    print(f"{num_words} words found in the document\n")
+    for char in sorted_chars_dict:
+        print(f"The '{char["char"]}' character was found {char["num"]} times")
+
+
+    print("--- End report --")
+    
+def main():
+    book_path = "/root/workspace/github.com/JakubKafel/bookbot/books/frankenstein.txt"
+    text = get_book_text(book_path)
+    #num_words = get_num_words(text)
+    #print(f"{num_words} words found in the document")
+    #charater_dictionary = count_characters(text)
+    #print(charater_dictionary)
+
+    report(text,book_path)
 
 main()
